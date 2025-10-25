158ddc8a9d0b77baaccfbfad2d80bed96845dcbe
Nicholas Killewald <captainspam@gmail.com>
2025-10-25 12:51:00 -0700

    Created BaseView and NormalView.
    The idea I'm going for is that any given sequence on the display will be
    a "view".  Views get called on each frame to determine what needs to go
    on the display (besides color changes).  It's that simple.
    
    This creates BaseView (the base class, with helper methods) and
    NormalView (the "idle" state of the project, displaying current run
    stats).

---
+++ desertbus/base_view.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+import time
+import adafruit_character_lcd.character_lcd as characterlcd
+
+class BaseView(ABC):
+    """The basic abstract view from which other views derive."""
+    def __init__(self, lcd: characterlcd.Character_LCD):
+        self._advance_frame_time()
+        self._lcd = lcd
+
+    @property
+    @abstractmethod
+    def priority(self):
+        """The view's priority.  Lower numbers mean higher priority."""
+        pass
+
+    def _advance_frame_time(self):
+        """Advances the frame.  Which just means update the most recent frame
+        timestamp."""
+        self._last_frame_millis = round(time.time() * 1000)
+
+    def _display_text(self, line1, line2):
+        """Convenience method for displaying text to the screen."""
+        self._lcd.message = line1 + "\n" + line2
+
+    def __lt__(self, other):
+        if not isinstance(other, BaseView):
+            return NotImplemented
+        return self.priority < other.priority
+
+    @abstractmethod
+    def next_frame(self, data: any) -> bool:
+        """Process the next frame.  Exactly what this means is up to the
+        implementation; it might just be repeating the same strings currently
+        on-screen.
+
+        There is no guarantee that a given view will handle the next frame that
+        needs processing, even if the view isn't finished yet.  Any view can be
+        interrupted by something of a higher (lower-numbered) priority, in which
+        case the lower-priority view just won't get any next_frame calls until
+        the higher-priority view finishes and yields control.  In fact, that's
+        the entire way NormalView is intended to work.
+
+        Returns True if this view is complete and should be disposed
+        afterward (i.e. a transition animation is complete), False if there's
+        still more to go (i.e. the animation isn't complete, or this is
+        NormalView and shouldn't ever go away)."""
+        pass
---
+++ desertbus/normal_view.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+from desertbus.base_view import BaseView
+from desertbus.vst_data import VstData
+import time
+import adafruit_character_lcd.character_lcd as characterlcd
+
+def _route_page(data: VstData) -> str:
+    if data.is_going_to_tucson:
+        return "Tucson -> Vegas"
+    else:
+        return "Vegas -> Tucson"
+
+def _to_next_hour_page(data: VstData) -> str:
+    return f"Next: ${data.to_next_hour:,.2f}"
+
+def _hours_bussed_page(data: VstData) -> str:
+    return f"Bussed: {data.hours_bussed}:{data.minutes_bussed}"
+
+def _total_hours_page(data: VstData) -> str:
+    return f"Total hours: {data.total_hours}"
+
+def _pt_cr_page(data: VstData) -> str:
+    return f"PT:CR: {data.points}:{data.crashes}"
+
+def _total_splats_page(data: VstData) -> str:
+    return f"Bug splats: {data.splats}"
+
+def _total_stops_page(data: VstData) -> str:
+    return f"Bus stops: {data.stops}"
+
+_PAGES = [
+    _route_page,
+    _to_next_hour_page,
+    _hours_bussed_page,
+    _total_hours_page,
+    _pt_cr_page,
+    _total_splats_page,
+    _total_stops_page,
+]
+
+_TOTAL_PAGES = len(_PAGES)
+
+class NormalView(BaseView):
+    """The NormalView, NormalView, NormalView, NORMALVIEEEEEEEEEEEEEW!!! is the
+    default "idle" state of the display.  It displays the current donation total
+    on the lower half of the display and cycles through different stats on the
+    top half."""
+    def __init__(self, lcd: characterlcd.Character_LCD):
+        super().__init__(lcd)
+        # The last time we swapped data pages.
+        self._start_time = time.time()
+
+    @property
+    def priority(self):
+        # NormalView should be the lowest-priority view.  Anything should 
+        # outrank it.  Obviously, Python has no concept of a MAX_INT, but still,
+        # NormalView should have a higher number than anything else in the
+        # system.
+        return 9999
+
+    def next_frame(self, data: any) -> bool:
+        # This really super needs to have a VstData to do something.
+        if not isinstance(data, VstData):
+            return False
+
+        # Determine what page we're on based on elapsed time.
+        current_page = int(((time.time() - self._start_time) // 4) % _TOTAL_PAGES)
+        line1 = _PAGES[current_page](data).center(16)
+        line2 = f"${data.donation_total:,.2f}".center(16)
+        self._display_text(line1, line2)
+
+        # We don't use the frame timer, but still, advance it.
+        self._advance_frame_time()
+        return False
