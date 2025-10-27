aa9962790104a303a8d942c8069807094f4f18c3
Nicholas Killewald <captainspam@gmail.com>
2025-10-26 23:41:51 -0700

    Moved animation methods into their own view.
    SimpleAnimationView will now handle any simple animate-and-dispose
    views, like shift transitions and event animations.

--- desertbus/base_view.py
+++ desertbus/base_view.py
@@ -2,7 +2,6 @@
 
 from abc import ABC, abstractmethod
 import time
-from collections import deque
 import adafruit_character_lcd.character_lcd as characterlcd
 
 class BaseView(ABC):
@@ -10,61 +9,6 @@ class BaseView(ABC):
     def __init__(self, lcd: characterlcd.Character_LCD):
         self._advance_frame_time()
         self._lcd = lcd
-
-    def _prepare_animation(self, anim_list: list, start_time_millis=None) -> deque:
-        """Prepares a list of animation commands for execution.  Such a list
-        should be a series of tuples in the form of (time_millis, line1, line2),
-        where time_millis is the time (in millis) the frame should last, and
-        line1 and line2 are the two strings to be displayed.  This returns a
-        deque copy of that list where the first value in the tuple is each
-        real-world timestamp at which the animation should occur, assuming it
-        starts immediately, and should be fed into _do_animation_frame() to
-        execute the next frame as need be.  If you want to base it on some other
-        time, set start_time_millis accordingly."""
-        next_time_millis = start_time_millis
-        if next_time_millis is None:
-            next_time_millis = time.time() * 1000
-
-        result = deque()
-
-        for (time_millis, line1, line2) in anim_list:
-            if line1 is None or line2 is None:
-                raise ValueError("Animation lines can't be None!")
-            result.append((next_time_millis, line1, line2))
-            next_time_millis = next_time_millis + time_millis
-
-        # The last frame will have the time, but Nones for lines, to indicate it
-        # should wait for the last frame and then stop.
-        result.append((next_time_millis, None, None))
-
-        return result
-
-    def _do_animation_frame(self, anim_deque: deque) -> bool:
-        """Executes an animation frame.  Once a frame expires, it's removed from
-        the deque.  Once the deque is empty (or rather, once it runs into an
-        entry where the lines are Nones), this returns True.  Otherwise, this
-        returns False.  So, if this is a pure animation view (which most uses of
-        this will be), the result of _do_animation_frame() can generally be used
-        as the return value of next_frame().
-
-        Note that this DOES change anim_deque as it processes frames."""
-        current_time_millis = time.time() * 1000
-
-        # Step one, find the current frame.
-        current_frame = anim_deque[0]
-        while len(anim_deque) > 1 and anim_deque[1][0] < current_time_millis:
-            # If the NEXT frame is due, remove the lead from the queue and try
-            # again.
-            current_frame = anim_deque.popleft()
-
-        if len(anim_deque) == 0 or current_frame[1] is None or current_frame[2] is None:
-            # If the deque is empty at this point or we've reached the None/None
-            # sentinel value, we're done with the animation.
-            return True
-
-        # Display the frame!
-        self._display_text(current_frame[1], current_frame[2])
-        return False
 
     @property
     @abstractmethod
---
+++ desertbus/simple_animation_view.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+from desertbus.base_view import BaseView
+import logging
+import time
+from collections import deque
+import adafruit_character_lcd.character_lcd as characterlcd
+
+logger = logging.getLogger(__name__)
+
+class SimpleAnimationView(BaseView):
+    """A view that does a simple animation, then exits.
+
+    This depends heavily on the anim_sequence parameter.  That's a list of
+    three-element tuples in the form (duration_millis, line1, line2), where
+    duration_millis is the time (in millis) the frame should last, and line1 and
+    line2 are the two lines to be displayed."""
+    def __init__(self,
+                 lcd: characterlcd.Character_LCD,
+                 anim_sequence: list,
+                 name: str="Unnamed Animation",
+                 priority: int=10):
+        super().__init__(lcd)
+        logger.info(f"Initializing SimpleAnimationView ({name})!")
+        self._name = name
+        self._priority = priority
+        self._anim_sequence = anim_sequence
+        self._anim_deque = None
+
+    @property
+    def priority(self):
+        return self._priority
+    
+    @property
+    def name(self):
+        return f'SimpleAnimationView ({self._name})'
+    
+    def _prepare_animation(self, anim_list: list, start_time_millis=None) -> deque:
+        """Prepares a list of animation commands for execution.  This returns a
+        deque copy of the list where the first value in the tuple is each
+        real-world timestamp at which the animation should occur, assuming it
+        starts immediately, and should be fed into _do_animation_frame() to
+        execute the next frame as need be.  If you want to base it on some other
+        time, set start_time_millis accordingly."""
+        next_time_millis = start_time_millis
+        if next_time_millis is None:
+            next_time_millis = time.time() * 1000
+
+        result = deque()
+
+        for (time_millis, line1, line2) in anim_list:
+            if line1 is None or line2 is None:
+                raise ValueError("Animation lines can't be None!")
+            result.append((next_time_millis, line1, line2))
+            next_time_millis = next_time_millis + time_millis
+
+        # The last frame will have the time, but Nones for lines, to indicate it
+        # should wait for the last frame and then stop.
+        result.append((next_time_millis, None, None))
+
+        return result
+
+    def _do_animation_frame(self, anim_deque: deque) -> bool:
+        """Executes an animation frame.  Once a frame expires, it's removed from
+        the deque.  Once the deque is empty (or rather, once it runs into an
+        entry where the lines are Nones), this returns True.  Otherwise, this
+        returns False.  So, if this is a pure animation view (which most uses of
+        this will be), the result of _do_animation_frame() can generally be used
+        as the return value of next_frame().
+
+        Note that this DOES change anim_deque as it processes frames."""
+        current_time_millis = time.time() * 1000
+
+        # Step one, find the current frame.
+        current_frame = anim_deque[0]
+        while len(anim_deque) > 1 and anim_deque[1][0] < current_time_millis:
+            # If the NEXT frame is due, remove the lead from the queue and try
+            # again.
+            current_frame = anim_deque.popleft()
+
+        if len(anim_deque) == 0 or current_frame[1] is None or current_frame[2] is None:
+            # If the deque is empty at this point or we've reached the None/None
+            # sentinel value, we're done with the animation.
+            return True
+
+        # Display the frame!
+        self._display_text(current_frame[1], current_frame[2])
+        return False
+
+    def next_frame(self, any) -> bool:
+        if self._anim_deque is None:
+            self._anim_deque = self._prepare_animation(self._anim_sequence)
+
+        return self._do_animation_frame(self._anim_deque)
