d8828c3fe8517d86b4e69ddb1ec56f78fdbfeac7
Nicholas Killewald <captainspam@gmail.com>
2025-10-26 17:52:59 -0700

    Added animating countup for donations.
    Now, any changes in donations will trigger a countup animation.  This
    includes initial startup, just so there's more interesting stuff going
    on right away.

--- desertbus/normal_view.py
+++ desertbus/normal_view.py
@@ -54,6 +54,7 @@ _TOTAL_OFFSEASON_PAGES = len(_OFFSEASON_PAGES)
 _SERVICE_DOT_MILLIS = 120000
 _LIVE_PAGE_TIME_SECS = 4
 _OFFSEASON_PAGE_TIME_SECS = 6
+_COUNTUP_ANIMATION_MILLIS = 1000
 
 def _needs_service_dot(data: VstData) -> bool:
     time_since_last = (time.time() * 1000) - data.time_fetched
@@ -70,6 +71,17 @@ class NormalView(BaseView):
         logger.info("Initializing NormalView!")
         self._start_time = time.time()
 
+        # The last "stabilized" donation total, meaning the last time a final
+        # count was displayed.  This will only be different from the last
+        # displayed count or what's in the incoming data when updating to a
+        # new value and in the middle of an animation.
+        self._last_stabilized_donations = 0
+        # The last time we had a stabilized donation total.  This is used to
+        # calculate how much we need to increase the amount per frame during an
+        # animation.  This initializes to zero and not self._start_time * 1000
+        # to make sure we're starting at the first actual frame, not at init.
+        self._last_stabilized_time_millis = 0
+
     @property
     def priority(self):
         # NormalView should be the lowest-priority view.  Anything should 
@@ -78,10 +90,45 @@ class NormalView(BaseView):
         # system.
         return 9999
 
+    def _get_displayed_donation_total(self, data: VstData) -> float:
+        """Gets the displayed donation total, accounting for any potential
+        animation currently active."""
+        right_now_millis = time.time() * 1000
+
+        if self._last_stabilized_donations == data.donation_total:
+            # We're stabilized; update the counter and return the total.
+            logger.debug(f'Donations stable at ${data.donation_total}.')
+            self._last_stabilized_time_millis = right_now_millis
+            return data.donation_total
+        else:
+            # Otherwise, an animation is in progress (or we're about to start
+            # one).  First, get how much time has elapsed, then how far along we
+            # are in the animation.
+            elapsed_time = right_now_millis - self._last_stabilized_time_millis
+            progress_percent = elapsed_time / _COUNTUP_ANIMATION_MILLIS
+
+            if progress_percent >= 1.0:
+                # We've reached the end!  Display the actual total and
+                # stabilize.
+                logger.info(f'Ending animation at ${data.donation_total}.')
+                self._last_stabilized_time_millis = right_now_millis
+                self._last_stabilized_donations = data.donation_total
+                return data.donation_total
+            else:
+                # We're animating!  Figure out what needs to be displayed.
+                donation_difference = data.donation_total - self._last_stabilized_donations
+                fractional_delta = donation_difference * progress_percent
+                logger.debug(f'Animating from ${self._last_stabilized_donations} to ${data.donation_total}; displaying ${round(self._last_stabilized_donations + fractional_delta, 2)}...')
+                return round(self._last_stabilized_donations + fractional_delta, 2)
+
     def next_frame(self, data: any) -> bool:
         # This really super needs to have a VstData to do something.
         if not isinstance(data, VstData):
             return False
+
+        if self._last_stabilized_time_millis == 0:
+            # This is the first frame!  Initialize!
+            self._last_stabilized_time_millis = time.time() * 1000
 
         line1 = ''
         time_delta = time.time() - self._start_time
@@ -95,7 +142,7 @@ class NormalView(BaseView):
             line1 = _OFFSEASON_PAGES[current_page](data).center(16)
 
         # Either way, the second line is the donation total.
-        line2 = f"${data.donation_total:,.2f}{'.' if _needs_service_dot(data) else ''}".center(16)
+        line2 = f"${self._get_displayed_donation_total(data):,.2f}{'.' if _needs_service_dot(data) else ''}".center(16)
 
         # Display 'em!
         self._display_text(line1, line2)
