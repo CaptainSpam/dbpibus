5f1e552f95a34b4ef2a2a95a58d28510c3730e7a
Nicholas Killewald <captainspam@gmail.com>
2025-10-26 23:57:42 -0700

    Swapped to the v2 JSON stats file.
    Sokar's new v2 JSON stats are live!  Hopefully.  I'll need to get this
    into the Android widget later, too.

--- desertbus/fetcher.py
+++ desertbus/fetcher.py
@@ -16,14 +16,21 @@ _URL_PREFIX = 'https://vst.ninja/'
 _IS_OMEGA_URL = f'{_URL_PREFIX}Resources/isitomegashift.html'
 
 # Field names are mostly tentative.
-_JSON_ODOMETER = 'Current Mileage'
-_JSON_POINTS = 'Points: Total'
-_JSON_CRASHES = 'Crashes: Total'
-_JSON_SPLATS = 'Bug Splats: Total'
-_JSON_STOPS = 'Bus Stops: Total'
+_JSON_STATS_CATEGORY = "Stats"
+
+_JSON_GAME_DATA_CATEGORY = "Game Data"
+_JSON_ODOMETER = 'Desert Bus: Current Mileage'
+_JSON_POINTS = 'Desert Bus: Total Points'
+_JSON_CRASHES = 'Desert Bus: Total Crashes'
+_JSON_SPLATS = 'Desert Bus: Total Bug Splats'
+_JSON_STOPS = 'Desert Bus: Total Bus Stops'
+
+_JSON_YEAR_DATA_CAGEGORY = "Year Data"
 _JSON_IS_LIVE = 'Run Live'
-_JSON_DONATIONS = 'Total Raised'
 _JSON_RUN_START_TIME = 'Year Start UNIX-Time'
+
+_JSON_DONATION_DATA_CATEGORY = "Donation Data"
+_JSON_DONATIONS = 'Total Raised'
 
 _YEAR_OFFSET = 2006
 # Floats should be okay here, unless Python has issues with the hundredths digit.
@@ -35,7 +42,7 @@ _MILLIS_PER_HOUR = _MILLIS_PER_MINUTE * 60
 
 def _make_stats_url_for_year(year):
     numbered_run = year - _YEAR_OFFSET
-    return f'{_URL_PREFIX}DB{numbered_run}/data/DB{numbered_run}_stats.json'
+    return f'{_URL_PREFIX}DB{numbered_run}/data/DB{numbered_run}_stats_v2.json'
 
 def get_current_stats() -> VstData:
     """Fetches the current stats from the VST."""
@@ -78,25 +85,30 @@ def get_current_stats() -> VstData:
 
     # Now we've got data!  Let's get it parsed!  Make it its own method to keep
     # things tidy and well-organized.
-    return _parse_stats(json_data[0], omega)
+    return _parse_stats(json_data, omega)
 
 def _parse_stats(json_blob, omega: bool) -> VstData:
     """Parses the raw VST results into a VstData object."""
     logger.debug(f'Parsing data blob: {json_blob}')
+    stats = json_blob.get(_JSON_STATS_CATEGORY)
+    year_data = stats.get(_JSON_YEAR_DATA_CAGEGORY)
+    game_data = stats.get(_JSON_GAME_DATA_CATEGORY)
+    donation_data = stats.get(_JSON_DONATION_DATA_CATEGORY)
+
     # There isn't much processing we need to do, but there IS something.
-    miles_total = float(json_blob.get(_JSON_ODOMETER, 0.0))
+    miles_total = float(game_data.get(_JSON_ODOMETER, 0.0))
     miles_driven = miles_total - _ODOMETER_OFFSET
     trips_taken = miles_driven // _MILES_TO_VEGAS
     is_going_to_tucson = False
     if trips_taken >= 0 and trips_taken % 2 == 1:
         is_going_to_tucson = True
 
-    start_time_millis = json_blob.get(_JSON_RUN_START_TIME, 0) * 1000
+    start_time_millis = year_data.get(_JSON_RUN_START_TIME, 0) * 1000
     right_now_millis = round(time.time() * 1000)
     hours_bussed = (right_now_millis - start_time_millis) // _MILLIS_PER_HOUR
     minutes_bussed = ((right_now_millis - start_time_millis) % _MILLIS_PER_HOUR) // _MILLIS_PER_MINUTE
 
-    donation_total = float(json_blob.get(_JSON_DONATIONS, 0.0))
+    donation_total = float(donation_data.get(_JSON_DONATIONS, 0.0))
     to_next_hour = donation_converter.to_next_hour_from_donation_amount(donation_total)
     total_hours = donation_converter.total_hours_for_donation_amount(donation_total)
 
@@ -107,18 +119,18 @@ def _parse_stats(json_blob, omega: bool) -> VstData:
         current_shift = get_current_shift()
 
     return VstData(time_fetched = right_now_millis,
-                   donation_total = float(json_blob.get(_JSON_DONATIONS, 0.0)),
+                   donation_total = donation_total,
                    hours_bussed = hours_bussed,
                    minutes_bussed = minutes_bussed,
                    to_next_hour = to_next_hour,
                    total_hours = total_hours,
                    odometer = miles_total,
-                   points = json_blob.get(_JSON_POINTS, 0),
-                   crashes = json_blob.get(_JSON_CRASHES, 0),
-                   splats = json_blob.get(_JSON_SPLATS, 0),
-                   stops = json_blob.get(_JSON_STOPS, 0),
+                   points = game_data.get(_JSON_POINTS, 0),
+                   crashes = game_data.get(_JSON_CRASHES, 0),
+                   splats = game_data.get(_JSON_SPLATS, 0),
+                   stops = game_data.get(_JSON_STOPS, 0),
                    current_shift = current_shift,
-                   is_live = bool(json_blob.get(_JSON_IS_LIVE, False)),
+                   is_live = bool(year_data.get(_JSON_IS_LIVE, False)),
                    is_omega_shift = omega,
                    is_going_to_tucson = is_going_to_tucson)
 
