452168c69ec02f04887b0ae0187bea1936e3b447
Nicholas Killewald <captainspam@gmail.com>
2025-10-27 23:21:25 -0700

    Added animation data for Dawn Guard and Zeta Shift.
    Zeta's been more fleshed out, and Dawn Guard now has the entire theme.
    The timing might be a bit off on Dawn Guard, but it should do for now.
    
    Also, this adds a helper function to generate an appropriate
    SimpleAnimationView for each shift, once I've got them all finished.

--- desertbus/shift_data.py
+++ desertbus/shift_data.py
@@ -5,6 +5,7 @@
 from enum import Enum, auto
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from desertbus.simple_animation_view import SimpleAnimationView
 
 _PACIFIC_ZONEINFO = ZoneInfo('America/Los_Angeles')
 
@@ -40,32 +41,151 @@ def get_current_shift() -> Shift:
     return Shift.NIGHT_WATCH
 
 # Shift animation sequences.
+DAWN_GUARD_ANIM = [
+    (500, " DAWN           ","                "),
+    (500, " DAWN DAWN      ","                "),
+    (500, " DAWN DAWN DAWN ","                "),
+    (500, " DAWN DAWN DAWN ","  DAWN          "),
+    (125, " DAWN DAWN DAWN ","  DAWN DA-      "),
+    (825, " DAWN DAWN DAWN ","  DAWN DA-DAWN  "),
+    (500, " DAWN           ","                "),
+    (125, " DAWN DA-       ","                "),
+    (825, " DAWN DA-DAWN   ","                "),
+
+    (500, " DAWN           ","                "),
+    (500, " DAWN DAWN      ","                "),
+    (500, " DAWN DAWN DAWN ","                "),
+    (500, " DAWN DAWN DAWN ","  DAWN          "),
+    (125, " DAWN DAWN DAWN ","  DAWN DA-      "),
+    (825, " DAWN DAWN DAWN ","  DAWN DA-DAWN  "),
+    (500, " DAWN           ","                "),
+    (125, " DAWN DA-       ","                "),
+    (825, " DAWN DA-DAWN   ","                "),
+
+    (500, " DAWN           ","                "),
+    (500, " DAWN DAWN      ","                "),
+    (125, " DAWN DAWN DA-  ","                "),
+    (825, " DAWN           ","                "),
+    (500, " DAWN DAWN      ","                "),
+    (125, " DAWN DAWN      "," DA-            "),
+    (125, " DAWN DAWN      "," DA-DA-         "),
+    (125, " DAWN DAWN      "," DA-DA-DA-      "),
+    (825, " DAWN DAWN      "," DA-DA-DA-DAWN  "),
+
+    (250, " DAWN           ","                "),
+    (750, " DAWN DAWN      ","                "),
+    (500, " DAWN DAWN DAWN ","                "),
+    (125, " DAWN DAWN DAWN "," DA-            "),
+    (125, " DAWN DAWN DAWN "," DA-DA-         "),
+    (125, " DAWN DAWN DAWN "," DA-DA-DA-      "),
+    (825, " DAWN DAWN DAWN "," DA-DA-DA-DAWN  "),
+
+    (250, " DAWN           ","                "),
+    (750, " DAWN DAWN      ","                "),
+    (500, " DAWN DAWN DAWN ","                "),
+    (125, " DAWN DAWN DAWN "," DA-            "),
+    (825, " DAWN DAWN DAWN "," DA-DAWN        "),
+    (500, " DAWN           ","                "),
+    (125, " DAWN DA-       ","                "),
+    (825, " DAWN DA-DAWN   ","                "),
+
+    (500, " DAWN           ","                "),
+    (500, " DAWN DAWN      ","                "),
+    (125, " DAWN DAWN DA-  ","                "),
+    (825, " DAWN           ","                "),
+    (500, " DAWN DAWN      ","                "),
+    (125, " DAWN DAWN      "," DA-            "),
+    (125, " DAWN DAWN      "," DA-DA-         "),
+    (125, " DAWN DAWN      "," DA-DA-DA-      "),
+    (825, " DAWN DAWN      "," DA-DA-DA-DAWN  "),
+
+    (250, " DAWN           ","                "),
+    (750, " DAWN DAWN      ","                "),
+    (500, " DAWN DAWN DAWN ","                "),
+    (125, " DAWN DAWN DAWN "," DA-            "),
+    (125, " DAWN DAWN DAWN "," DA-DA-         "),
+    (125, " DAWN DAWN DAWN "," DA-DA-DA-      "),
+    (825, " DAWN DAWN DAWN "," DA-DA-DA-DAWN  "),
+
+    (250, " DAWN           ","                "),
+    (750, " DAWN DAWN      ","                "),
+    (500, " DAWN DAWN DAWN ","                "),
+    (125, " DAWN DAWN DAWN "," DA-            "),
+    (825, " DAWN DAWN DAWN "," DA-DAWN        "),
+    (500, " DAWN           ","                "),
+    (125, "   DAWN         ","                "),
+    (5000,"   DAWN GUARD   ","                "),
+]
+
 ZETA_SHIFT_ANIM = [
-    (100, "L".center(16),"                "),
-    (100, "LL".center(16),"                "),
-    (100, "LLL".center(16),"                "),
-    (100, "LLLL".center(16),"                "),
-    (100, "LLLLL".center(16),"                "),
-    (100, "LLLLLL".center(16),"                "),
-    (100, "LLLLLLL".center(16),"                "),
-    (100, "LLLLLLLL".center(16),"                "),
-    (100, "LLLLLLLLL".center(16),"                "),
-    (350, "LET'S           ","                "),
-    (350, "LET'S GET       ","                "),
-    (350, "LET'S GET READY ","                "),
-    (200 ,"LET'S GET READY ","  TO            "),
-    (1500,"LET'S GET READY ","  TO RUMBLE!!!  "),
+    (75,  "L               ","                "),
+    (75,  "LL              ","                "),
+    (75,  "LLL             ","                "),
+    (75,  "LLLL            ","                "),
+    (75,  "LLLLL           ","                "),
+    (75,  "LLLLLL          ","                "),
+    (75,  "LLLLLLL         ","                "),
+    (75,  "LLLLLLLL        ","                "),
+    (75,  "LLLLLLLLL       ","                "),
+    (75,  "LLLLLLLLLL      ","                "),
+    (75,  "LLLLLLLLLLL     ","                "),
+    (75,  "LLLLLLLLLLLL    ","                "),
+    (75,  "LLLLLLLLLLLLL   ","                "),
+    (75,  "LLLLLLLLLLLLLL  ","                "),
+    (75,  "LLLLLLLLLLLLLLL ","                "),
+    (75,  "LLLLLLLLLLLLLLLL","                "),
+    (350, "Let's           ","                "),
+    (350, "Let's get       ","                "),
+    (350, "Let's get ready ","                "),
+    (200 ,"Let's get ready ","  to            "),
+    (1500,"Let's get ready ","  to RUMBLE!!!  "),
     (500, "ZETA            ","            ZETA"),
     (500, "    ZETA        ","        ZETA    "),
     (500, "        ZETA    ","    ZETA        "),
     (500, "            ZETA","ZETA            "),
-    (100,  "ZETA            ","                "),
-    (100,  "                ","    ZETA        "),
-    (100,  "        ZETA    ","                "),
-    (100,  "                ","            ZETA"),
-    (100,  "                ","ZETA            "),
-    (100,  "    ZETA        ","                "),
-    (100,  "                ","        ZETA    "),
-    (100,  "            ZETA","                "),
-    (5000,"ZETA SHIFT".center(16),"                "),
+    (250, "ZETA            ","            ZETA"),
+    (250, "ZETAZETA        ","        ZETAZETA"),
+    (250, "ZETAZETAZETA    ","    ZETAZETAZETA"),
+    (250, "ZETAZETAZETAZETA","ZETAZETAZETAZETA"),
+    (250, "    ZETAZETAZETA","ZETAZETAZETA    "),
+    (250, "        ZETAZETA","ZETAZETA        "),
+    (250, "            ZETA","ZETA            "),
+    (250, "                ","                "),
+    (250, "ZETA  Z   Z   Z ","  Z   Z   Z   Z "),
+    (250, "  Z   Z   Z   Z ","  Z   Z ZETA  Z "),
+    (250, "  Z   Z   Z ZETA","  Z   Z   Z   Z "),
+    (250, "  Z   Z   Z   Z ","ZETA  Z   Z   Z "),
+    (250, "  Z   Z ZETA  Z ","  Z   Z   Z   Z "),
+    (250, "  Z   Z   Z   Z ","  Z ZETA  Z   Z "),
+    (250, "  Z ZETA  Z   Z ","  Z   Z   Z   Z "),
+    (250, "  Z   Z   Z   Z ","  Z   Z   Z ZETA"),
+    (250, "    < ZETA >    ", "                "),
+    (250, "                ", "   << ZETA >>   "),
+    (250, "  <<< ZETA >>>  ", "                "),
+    (250, "                ", " <<<< ZETA >>>> "),
+    (250, " <<<< ZETA >>>> ", "                "),
+    (250, "                ", "  <<< ZETA >>>  "),
+    (250, "   << ZETA >>   ", "                "),
+    (250, "                ", "    < ZETA >    "),
+    (300, "  Y'all         ", "                "),
+    (300, "  Y'all ready   ", "                "),
+    (150, "  Y'all ready   ", "    for         "),
+    (1200,"  Y'all ready   ", "    for this?   "),
+    (5000,"   ZETA SHIFT   ", "                "),
 ]
+
+def make_view_for_shift(lcd, shift: Shift) -> SimpleAnimationView:
+    match(shift):
+        # TODO: Implement the rest.
+        case Shift.DAWN_GUARD:
+            return SimpleAnimationView(lcd, DAWN_GUARD_ANIM, "Dawn Transition", 5)
+        case Shift.ALPHA_FLIGHT:
+            return None
+        case Shift.NIGHT_WATCH:
+            return None
+        case Shift.OMEGA_SHIFT:
+            return None
+        case Shift.ZETA_SHIFT:
+            return SimpleAnimationView(lcd, ZETA_SHIFT_ANIM, "Zeta Transition", 5)
+        case _:
+            raise ValueError(f"Invalid value passed to make_view_for_shift(): {shift}")
