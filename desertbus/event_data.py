5cf0a5472fb934d6cb6ce9c78759725990c7cb85
Nicholas Killewald <captainspam@gmail.com>
2025-11-02 14:05:39 -0800

    Added remaining events to event_data.py.
    This should be the last of the animations we'll need.  Yay!

--- desertbus/event_data.py
+++ desertbus/event_data.py
@@ -7,42 +7,104 @@ from desertbus.simple_animation_view import SimpleAnimationView
 from desertbus.vst_data import VstData
 
 POINT_GET_ANIM = [
-    (100, "   -            ", "                "),
-    (100, "   +            ", "                "),
-    (100, "   P|           ", "                "),
-    (100, "   P+           ", "                "),
-    (100, "   PO-          ", "                "),
-    (100, "   PO+          ", "                "),
-    (100, "   POI|         ", "                "),
-    (100, "   POI+         ", "                "),
-    (100, "   POIN-        ", "                "),
-    (100, "   POIN+        ", "                "),
-    (100, "   POINT |      ", "                "),
-    (100, "   POINT +      ", "                "),
-    (100, "   POINT G-     ", "                "),
-    (100, "   POINT G+     ", "                "),
-    (100, "   POINT GE|    ", "                "),
-    (100, "   POINT GE+    ", "                "),
-    (100, "   POINT GET-   ", "                "),
-    (100, "   POINT GET+   ", "                "),
-    (500, "   POINT GET!   ", "                "),
-    (100, "   POINT GET!   ", "  .             "),
-    (100, "   POINT GET!   ", "  +       .     "),
-    (100, "   POINT GET!   ", "  *   .   +     "),
-    (100, "   POINT GET!   ", "      +   *   . "),
-    (100, "   POINT GET!   ", " .    *       + "),
-    (100, "   POINT GET!   ", " +      .     * "),
-    (100, "   POINT GET!   ", " *      +   .   "),
-    (100, "   POINT GET!   ", "    .   *   +   "),
-    (100, "   POINT GET!   ", ".   +       *   "),
-    (100, "   POINT GET!   ", "+   *  .        "),
-    (100, "   POINT GET!   ", "*      +     .  "),
-    (100, "   POINT GET!   ", "   .   *     +  "),
-    (100, "   POINT GET!   ", "   +      .  *  "),
-    (100, "   POINT GET!   ", "   *      +    ."),
-    (100, "   POINT GET!   ", "          *    +"),
-    (100, "   POINT GET!   ", "               *"),
-    (3000,"   POINT GET!   ", "                "),
+    (100, "   -            ","                "),
+    (100, "   +            ","                "),
+    (100, "   P|           ","                "),
+    (100, "   P+           ","                "),
+    (100, "   PO-          ","                "),
+    (100, "   PO+          ","                "),
+    (100, "   POI|         ","                "),
+    (100, "   POI+         ","                "),
+    (100, "   POIN-        ","                "),
+    (100, "   POIN+        ","                "),
+    (100, "   POINT |      ","                "),
+    (100, "   POINT +      ","                "),
+    (100, "   POINT G-     ","                "),
+    (100, "   POINT G+     ","                "),
+    (100, "   POINT GE|    ","                "),
+    (100, "   POINT GE+    ","                "),
+    (100, "   POINT GET-   ","                "),
+    (100, "   POINT GET+   ","                "),
+    (500, "   POINT GET!   ","                "),
+    (100, "   POINT GET!   ","  .             "),
+    (100, "   POINT GET!   ","  +       .     "),
+    (100, "   POINT GET!   ","  *   .   +     "),
+    (100, "   POINT GET!   ","      +   *   . "),
+    (100, "   POINT GET!   "," .    *       + "),
+    (100, "   POINT GET!   "," +      .     * "),
+    (100, "   POINT GET!   "," *      +   .   "),
+    (100, "   POINT GET!   ","    .   *   +   "),
+    (100, "   POINT GET!   ",".   +       *   "),
+    (100, "   POINT GET!   ","+   *  .        "),
+    (100, "   POINT GET!   ","*      +     .  "),
+    (100, "   POINT GET!   ","   .   *     +  "),
+    (100, "   POINT GET!   ","   +      .  *  "),
+    (100, "   POINT GET!   ","   *      +    ."),
+    (100, "   POINT GET!   ","          *    +"),
+    (100, "   POINT GET!   ","               *"),
+    (3000,"   POINT GET!   ","                "),
+]
+
+BUG_SPLAT_ANIM = [
+    (100, "        S       ","                "),
+    (100, "        SP      ","                "),
+    (100, "     G  SPL     ","                "),
+    (100, "    UG  SPLA    ","                "),
+    (100, "   BUG  SPLAT   ","                "),
+    (100, "  *BUG  SPLAT*  ","                "),
+    (100, " * BUG  SPLAT * ","                "),
+    (100, "*  BUG  SPLAT  *","                "),
+    (1000,"   BUG  SPLAT   ","                "),
+    (300, "   BUG  SPLAT   ","     |          "),
+    (100, "   BUG  SPLAT   ","     |      |   "),
+    (200, "   BUG  SPLAT   ","     *      |   "),
+    (300, "   BUG  SPLAT   ","     *      *   "),
+    (400, "   BUG  SPLAT   ","     @   |  *   "),
+    (2600,"   BUG  SPLAT   ","   | @   *  @   "),
+]
+
+BUS_STOP_ANIM = [
+    (100, "               B","                "),
+    (150, "              BU","                "),
+    (400, "             BUS","                "),
+    (250, "            BUS ","                "),
+    (100, "           BUS S","                "),
+    (100, "         BUS STO","                "),
+    (100, "       BUS STOP ","                "),
+    (100, "     BUS STOP   ","                "),
+    (3000,"    BUS STOP    ","                "),
+    (100, "     BUS STOP   ","                "),
+    (100, "       BUS STOP ","                "),
+    (100, "         BUS STO","                "),
+    (100, "           BUS S","                "),
+    (250, "            BUS ","                "),
+    (400, "             BUS","                "),
+    (150, "              BU","                "),
+    (100, "               B","                "),
+    (100, "                ","                "),
+]
+
+CRASH_ANIM = [
+    (100, "A              S","                "),
+    (100, "RA            SH","                "),
+    (100, "CRA          SH!","                "),
+    (100, " CRA        SH! ","                "),
+    (100, "  CRA      SH!  ","                "),
+    (100, "   CRA    SH!   ","                "),
+    (100, "    CRA  SH!    ","                "),
+    (100, "     CRASH!     ","                "),
+    (100, "    <CRASH!>    ","                "),
+    (100, "   <<CRASH!>>   ","                "),
+    (100, "  << CRASH! >>  ","       .        "),
+    (100, " <<  CRASH!  >> ","      .  *      "),
+    (100, "<<  +CRASH!   >>","     .    *     "),
+    (100, "<  + CRASH!    >","    .      *    "),
+    (100, "  +  CRASH!.    ","   .        *   "),
+    (100, " +   CRASH! .   ","  .          *  "),
+    (100, "+    CRASH!  .  "," .            * "),
+    (100, "     CRASH!   . ",".              *"),
+    (100, "     CRASH!    .","                "),
+    (3000,"     CRASH!     ","                "),
 ]
 
 def make_views_for_events(lcd, prev_data: VstData, curr_data: VstData) -> List[SimpleAnimationView]:
@@ -58,9 +120,19 @@ def make_views_for_events(lcd, prev_data: VstData, curr_data: VstData) -> List[S
     if prev_data is not None and curr_data is not None:
         # We'll use priorities of 6-9 for events.  Shift changes should still
         # come first.
-        # TODO: Add the other events.
         if curr_data.points > prev_data.points:
             # POINT GET!
             events.append(SimpleAnimationView(lcd, POINT_GET_ANIM, "Point Get Animation", 6))
+        if curr_data.splats > prev_data.splats:
+            # Bug splat!
+            events.append(SimpleAnimationView(lcd, BUG_SPLAT_ANIM, "Bug Splat Animation", 7))
+        if curr_data.stops > prev_data.stops:
+            # Bus stop!
+            events.append(SimpleAnimationView(lcd, BUS_STOP_ANIM, "Bus Stop Animation", 8))
+        if curr_data.crashes > prev_data.crashes:
+            # CRASH!  This should get lowest priority, as it'll no doubt be the
+            # last thing that happens in any potential cluster of multiple
+            # simultaneous events.
+            events.append(SimpleAnimationView(lcd, CRASH_ANIM, "Crash Animation", 9))
 
     return events
