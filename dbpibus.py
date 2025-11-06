ac08ba211f04e06d522b1932a1bc0be53fa2ca19
Nicholas Killewald <captainspam@gmail.com>
2025-11-06 10:14:51 -0800

    Added top-level exception and interrupt handling and logging.
    Now the log will at least show when the program went down, if it does.

--- dbpibus.py
+++ dbpibus.py
@@ -26,6 +26,16 @@ from desertbus.event_data import make_views_for_events
 from desertbus.config import load_config, get_setting, ConfigKey, ShiftAnim, LcdColor, EventAnim
 from desertbus.service_menu_view import ServiceMenuView
 from desertbus.base_view import BaseView
+import signal
+import sys
+
+# Everybody do the interrupt!
+def interrupt_handler(signal, frame):
+    logger.info('Interrupt caught, shutting down...')
+    print('Interrput!  Shutting down...')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, interrupt_handler)
 
 def config_shift_to_data_shift(config_shift: LcdColor) -> Shift:
     """Converts the LcdColor config key to a Shift object.  I couldn't decide
@@ -114,109 +124,118 @@ is_aware_of_dead_fetcher_thread = False
 previous_stats = None
 last_known_lcd_setting = None
 
-while True:
-    latest_stats = t.latest_stats
-    current_lcd_setting = get_setting(ConfigKey.LCD_COLOR)
+# And now, presenting the entire main loop.  With a handy try-except block!
+try:
+    while True:
+        latest_stats = t.latest_stats
+        current_lcd_setting = get_setting(ConfigKey.LCD_COLOR)
 
-    # If the LCD color setting has suddenly changed, we need to update right
-    # away on this frame.
-    if not current_lcd_setting == last_known_lcd_setting:
-        if current_lcd_setting == LcdColor.CURRENT_SHIFT:
-            # If it's the current shift, work out what it is.
-            if latest_stats is not None and latest_stats.is_omega_shift:
-                lcd.color = SCREEN_COLORS[Shift.OMEGA_SHIFT]
+        # If the LCD color setting has suddenly changed, we need to update right
+        # away on this frame.
+        if not current_lcd_setting == last_known_lcd_setting:
+            if current_lcd_setting == LcdColor.CURRENT_SHIFT:
+                # If it's the current shift, work out what it is.
+                if latest_stats is not None and latest_stats.is_omega_shift:
+                    lcd.color = SCREEN_COLORS[Shift.OMEGA_SHIFT]
+                else:
+                    lcd.color = SCREEN_COLORS[get_current_shift()]
             else:
-                lcd.color = SCREEN_COLORS[get_current_shift()]
+                # If the setting is just a single shift, set it now.
+                lcd.color = SCREEN_COLORS[config_shift_to_data_shift(current_lcd_setting)]
+
+        # Check if we're in Omega yet.  Y'know, if there's stats at all.  Note
+        # that if the Omega flag is None, *don't change out of Omega*.  None
+        # means there was some problem fetching the Omega state, not that Omega
+        # is over (likely meaning we're in the offseason).  The ONLY way we
+        # leave Omega is if the flag is EXPLICITLY False.  If we were in Omega
+        # and got a None, stay in Omega.
+        now_shift = None
+        if (latest_stats is not None
+            and (latest_stats.is_omega_shift
+                or (latest_stats.is_omega_shift is None and now_shift == Shift.OMEGA_SHIFT))):
+            now_shift = Shift.OMEGA_SHIFT
         else:
-            # If the setting is just a single shift, set it now.
-            lcd.color = SCREEN_COLORS[config_shift_to_data_shift(current_lcd_setting)]
+            now_shift = get_current_shift()
+        if not now_shift == current_shift:
+            logger.info(f'Shift change!  Changing from {current_shift} to {now_shift}...')
 
-    # Check if we're in Omega yet.  Y'know, if there's stats at all.  Note that
-    # if the Omega flag is None, *don't change out of Omega*.  None means there
-    # was some problem fetching the Omega state, not that Omega is over (likely
-    # meaning we're in the offseason).  The ONLY way we leave Omega is if the
-    # flag is EXPLICITLY False.  If we were in Omega and got a None, stay in
-    # Omega.
-    now_shift = None
-    if (latest_stats is not None
-        and (latest_stats.is_omega_shift
-            or (latest_stats.is_omega_shift is None and now_shift == Shift.OMEGA_SHIFT))):
-        now_shift = Shift.OMEGA_SHIFT
-    else:
-        now_shift = get_current_shift()
-    if not now_shift == current_shift:
-        logger.info(f'Shift change!  Changing from {current_shift} to {now_shift}...')
+            # Set the screen color first (unless there's an override in config).
+            if current_lcd_setting == LcdColor.CURRENT_SHIFT:
+                lcd.color = SCREEN_COLORS[now_shift]
 
-        # Set the screen color first (unless there's an override in config).
-        if current_lcd_setting == LcdColor.CURRENT_SHIFT:
-            lcd.color = SCREEN_COLORS[now_shift]
+            # Then, ONLY if we're not switching OFF of Omega, do the animation.
+            # It just wouldn't be right to do a transition coming off of the end
+            # of the run.
+            #
+            # Also, y'know, check settings to make sure we should be doing this.
+            shift_anim_setting = get_setting(ConfigKey.SHOW_SHIFT_ANIM)
+            if (shift_anim_setting == ShiftAnim.ALWAYS
+                or (shift_anim_setting == ShiftAnim.ONLY_IN_SEASON
+                    and latest_stats is not None
+                    and latest_stats.is_live
+                    )) and not current_shift == Shift.OMEGA_SHIFT:
+                heapq.heappush(views, make_view_for_shift(lcd, now_shift))
 
-        # Then, ONLY if we're not switching OFF of Omega, do the animation.  It
-        # just wouldn't be right to do a transition coming off of the end of the
-        # run.
-        #
-        # Also, y'know, check settings to make sure we should be doing this.
-        shift_anim_setting = get_setting(ConfigKey.SHOW_SHIFT_ANIM)
-        if (shift_anim_setting == ShiftAnim.ALWAYS
-            or (shift_anim_setting == ShiftAnim.ONLY_IN_SEASON
-                and latest_stats is not None
-                and latest_stats.is_live
-                )) and not current_shift == Shift.OMEGA_SHIFT:
-            heapq.heappush(views, make_view_for_shift(lcd, now_shift))
+            current_shift = now_shift
+        if latest_stats is not None:
+            if len(views) == 0:
+                # If this is the first pass or if NormalView somehow got removed
+                # from the queue, put a new NormalView in.
+                heapq.heappush(views, NormalView(lcd))
 
-        current_shift = now_shift
-    if latest_stats is not None:
-        if len(views) == 0:
-            # If this is the first pass or if NormalView somehow got removed
-            # from the queue, put a new NormalView in.
-            heapq.heappush(views, NormalView(lcd))
+            # Handle any buttons first.
+            buttons = button_handler.get_button_state()
 
-        # Handle any buttons first.
-        buttons = button_handler.get_button_state()
+            button_result = views[0].handle_buttons(latest_stats, buttons)
 
-        button_result = views[0].handle_buttons(latest_stats, buttons)
+            if isinstance(button_result, BaseView):
+                # A view?  Someone must be running tests again.
+                heapq.heappush(views, button_result)
+            elif not button_result:
+                # The view didn't handle it (which is the most common case).
+                # It's up to us!
 
-        if isinstance(button_result, BaseView):
-            # A view?  Someone must be running tests again.
-            heapq.heappush(views, button_result)
-        elif not button_result:
-            # The view didn't handle it (which is the most common case).  It's
-            # up to us!
+                # First, check for the easter egg.  Only do this if the back
+                # button wasn't already pressed in the previous frame (so
+                # holding the back button won't just keep triggering
+                # ServiceCreditView).
+                if buttons.back and (previous_buttons is None or not previous_buttons.back):
+                    logger.info('Back was pressed outside of the menus, adding ServiceCreditView to the queue...')
+                    heapq.heappush(views, ServiceCreditView(lcd))
+                elif buttons.select and (previous_buttons is None or not previous_buttons.select):
+                    logger.info('Menu was pressed and not handled, adding ServiceMenuView to the queue...')
+                    heapq.heappush(views, ServiceMenuView(lcd))
 
-            # First, check for the easter egg.  Only do this if the back button
-            # wasn't already pressed in the previous frame (so holding back
-            # won't just keep triggering ServiceCreditView).
-            if buttons.back and (previous_buttons is None or not previous_buttons.back):
-                logger.info('Back was pressed outside of the menus, adding ServiceCreditView to the queue...')
-                heapq.heappush(views, ServiceCreditView(lcd))
-            elif buttons.select and (previous_buttons is None or not previous_buttons.select):
-                logger.info('Menu was pressed and not handled, adding ServiceMenuView to the queue...')
-                heapq.heappush(views, ServiceMenuView(lcd))
+            previous_buttons = buttons
 
-        previous_buttons = buttons
+            # Check for events (if the run is live); add those in if need be.
+            if latest_stats.is_live and get_setting(ConfigKey.SHOW_EVENT_ANIM) == EventAnim.ALWAYS:
+                event_views = make_views_for_events(lcd, previous_stats, latest_stats)
+                if len(event_views) > 0:
+                    for event_view in event_views:
+                        logger.info(f'Event occurred!  Adding {event_view} to the queue...')
+                        heapq.push(views, event_view)
 
-        # Check for events (if the run is live); add those in if need be.
-        if latest_stats.is_live and get_setting(ConfigKey.SHOW_EVENT_ANIM) == EventAnim.ALWAYS:
-            event_views = make_views_for_events(lcd, previous_stats, latest_stats)
-            if len(event_views) > 0:
-                for event_view in event_views:
-                    logger.info(f'Event occurred!  Adding {event_view} to the queue...')
-                    heapq.push(views, event_view)
+            # Then, handle the next frame.
+            if views[0].next_frame(latest_stats):
+                # This view just finished up, so pop it away.
+                logger.info(f'View {views[0].name} complete, removing from queue...')
+                heapq.heappop(views)
 
-        # Then, handle the next frame.
-        if views[0].next_frame(latest_stats):
-            # This view just finished up, so pop it away.
-            logger.info(f'View {views[0].name} complete, removing from queue...')
-            heapq.heappop(views)
+            # Stash away the previous stats and settings for the next check.
+            previous_stats = latest_stats
+            last_known_lcd_setting = current_lcd_setting
 
-        # Stash away the previous stats and settings for the next check.
-        previous_stats = latest_stats
-        last_known_lcd_setting = current_lcd_setting
+        if not t.is_alive() and not is_aware_of_dead_fetcher_thread:
+            logger.critical("THE FETCHER THREAD ISN'T RUNNING!  THIS IS REALLY BAD!")
+            # TODO: Maybe restart the thread?  This really, REALLY shouldn't
+            # happen, so there's a chance we're in a super bad state somehow.
+            is_aware_of_dead_fetcher_thread = True
 
-    if not t.is_alive() and not is_aware_of_dead_fetcher_thread:
-        logger.critical("THE FETCHER THREAD ISN'T RUNNING!  THIS IS REALLY BAD!")
-        # TODO: Maybe restart the thread?  This really, REALLY shouldn't happen,
-        # so there's a chance we're in a super bad state somehow.
-        is_aware_of_dead_fetcher_thread = True
-
-    sleep(0.035)
+        sleep(0.035)
+except Exception as e:
+    # Welp, we're hosed.
+    logger.exception('EXCEPTION!!!')
+    logger.critical('Shutting down...')
+    print('EXCEPTION!!! Shutting down...')
+    raise e
